066b58b6e57c1fb488906eba268a6c86cf06272a
Ira <152921559+Onsshuk2@users.noreply.github.com>
2024-10-18 15:10:10 +0100

    Add files via upload

---
+++ ForbiddenWordsFinder/ForbiddenWordsFinder/FileSearcher.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ForbiddenWordsFinder
+{
+    public class FileSearcher
+    {
+        public string DestinationPath { get; }
+
+        public FileSearcher(string destinationPath)
+        {
+            DestinationPath = destinationPath;
+        }
+
+        public void SearchFiles(string[] filePaths, List<string> bannedWords, IProgress<int> progress)
+        {
+            if (filePaths.Length == 0)
+            {
+                throw new ArgumentException("Немає файлів для пошуку.");
+            }
+
+            int totalFiles = filePaths.Length;
+            var report = new List<string>();
+            var bannedWordsCount = new Dictionary<string, int>();
+
+            for (int i = 0; i < totalFiles; i++)
+            {
+                string filePath = filePaths[i];
+
+                if (!File.Exists(filePath))
+                {
+                    progress.Report((i + 1) * 100 / totalFiles);
+                    continue;
+                }
+
+                string content = File.ReadAllText(filePath);
+                int replacements = 0;
+
+                foreach (var bannedWord in bannedWords)
+                {
+                    int count = content.Split(new[] { bannedWord }, StringSplitOptions.None).Length - 1;
+
+                    if (count > 0)
+                    {
+                        replacements += count;
+                        if (bannedWordsCount.ContainsKey(bannedWord))
+                            bannedWordsCount[bannedWord] += count;
+                        else
+                            bannedWordsCount[bannedWord] = count;
+
+                        // Заміна заборонених слів на зірочки
+                        content = content.Replace(bannedWord, new string('*', 7));
+                    }
+                }
+
+                // Запис модифікованого файлу
+                string modifiedFileName = Path.GetFileName(filePath);
+                string modifiedFilePath = Path.Combine(DestinationPath, "new file" + modifiedFileName);
+                File.WriteAllText(modifiedFilePath, content);
+
+                report.Add($"File: {filePath}, Replacements: {replacements}");
+
+                progress.Report((i + 1) * 100 / totalFiles);
+            }
+
+            // Запис звіту
+            string reportFilePath = Path.Combine(DestinationPath, "report.txt");
+            File.WriteAllLines(reportFilePath, report);
+
+           
+            var topBannedWords = bannedWordsCount.OrderByDescending(kvp => kvp.Value).Take(3);
+            using (StreamWriter sw = new StreamWriter(reportFilePath, true))
+            {
+                sw.WriteLine("\nTop 3 Banned Words:");
+                foreach (var item in topBannedWords)
+                {
+                    sw.WriteLine($"{item.Key}: {item.Value} times");
+                }
+            }
+        }
+    }
+}
---
+++ ForbiddenWordsFinder/ForbiddenWordsFinder/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace ForbiddenWordsFinder
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> selectedFilePaths = new List<string>();
+        private FileSearcher fileSearcher;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileSearcher = new FileSearcher("C:\\Users\\onssh\\OneDrive\\Робочий стіл");
+        }
+
+        private void LoadFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select Banned Words File",
+                Filter = "Text Files (*.txt)|*.txt",
+                Multiselect = false
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string bannedWordsContent = File.ReadAllText(openFileDialog.FileName);
+                txtBannedWords.Text = bannedWordsContent;
+            }
+        }
+
+        private void SelectFiles_Click(object sender, RoutedEventArgs e)
+        {
+            // вибір файлів
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select Files to Search",
+                Filter = "All Files (*.*)|*.*",
+                Multiselect = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                selectedFilePaths = openFileDialog.FileNames.ToList();
+                txtResults.Text = string.Join(Environment.NewLine, selectedFilePaths);
+            }
+        }
+
+        private async void Start_Click(object sender, RoutedEventArgs e)
+        {
+            // Отримання заборонених слів
+            List<string> bannedWords = txtBannedWords.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            // Перевірка на наявність заборонених слів
+            if (bannedWords.Count == 0)
+            {
+                MessageBox.Show("Будь ласка, введіть заборонені слова.");
+                return;
+            }
+
+            var progressIndicator = new Progress<int>(value => progressBar.Value = value);
+
+            // SearchFiles
+            await Task.Run(() => fileSearcher.SearchFiles(selectedFilePaths.ToArray(), bannedWords, progressIndicator));
+
+            MessageBox.Show("Пошук завершено!");
+        }
+    }
+
+}
